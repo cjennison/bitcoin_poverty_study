c3898dd18d2547b32d8f5828450687dc1d6c11cd
Christopher Jennison <cjennison92@gmail.com>
2016-03-17 14:12:09 -0400

    gets blocks from a given start date and day range

--- downloader.py
+++ downloader.py
@@ -1,9 +1,29 @@
 import requests
 import json
+from datetime import datetime, date, time, timedelta
+
+startYear = 2009
+startDay = 1;
+startMonth = 9;
+
+numberOfDays = 1;
+
+# Source; http://stackoverflow.com/questions/6999726/how-can-i-convert-a-datetime-object-to-milliseconds-since-epoch-unix-time-in-p
+date = datetime(startYear, startDay, startMonth)
+epoch = datetime.utcfromtimestamp(0)
+
+def unix_time_millis(dt):
+    return int((dt - epoch).total_seconds())*1000
 
 print "Downloading Data from Blockchain.info"
 
-url = 'https://blockchain.info/rawtx/129728510'
-response = requests.get(url)
 
-print response.text
+for i in range(0, 1):
+    date += timedelta(days=1);
+
+    #url = 'https://blockchain.info/rawtx/129728510'
+
+    # get blocks on this day
+    url = 'https://blockchain.info/blocks/' + str(unix_time_millis(date)) + '?format=json'
+    response = requests.get(url)
+    print response.text
