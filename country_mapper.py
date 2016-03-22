b398c2d1f36637692001c854960d844a8cd5d33a
Kali <rsbrisci@umass.edu>
2016-03-21 20:15:51 -0400

    country_mapper.py now returns the countries associated(sp) with the request address of each transaction

--- country_mapper.py
+++ country_mapper.py
@@ -1,16 +1,21 @@
 import sys
 import json
 import requests
-import pythonwhois
+import time
 
-if (len(sys.argv) < 3):
-    print "Invalid number of arguments (expected 2)"
+if (len(sys.argv) < 4):
+    print "Invalid number of arguments (expected 3)"
+    print "Usage:\n python country_mapper.py <transaction json file> <output file name> <country to export>"
     quit();
 
 print "Getting the initial IPs from ", str(sys.argv[1]);
 
 with open(sys.argv[1]) as json_data:
     txData = json.load(json_data);
+
+with open('config.json') as config_info:
+    configData = json.load(config_info);
+    otherapiKey = configData["otherapikey"];
 
 # We are going to append the inventory information to each transaction
 
@@ -22,11 +27,14 @@ print "Reading data from %d Transactions" % len(txData['transactions']);
 
 for i in range(0, len(txData['transactions'])):
     relayIP = txData['transactions'][i]['relayed_by'];
-    try:
-        countryIP = pythonwhois.get_whois(relayIP)
-        break
-    except shared.WhoisException:
-        countryIP = "WhoIsError"
-    print relayIP
+    #try:
+    if (i % 15 == 0):
+        time.sleep(1)
+    httpPacket = requests.get("http://api.ipinfodb.com/v3/ip-country/?key=" + str(otherapiKey) + "&ip=" + relayIP)
+    #except Exception:
+    #    countryIP = "WhoIsError"
+
+    countryIP = (httpPacket.text.split(";")[4])
+    print countryIP
 
     # TODO now find the IP information!
